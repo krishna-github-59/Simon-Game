db8f75d31714ac06469d8a18de2b8c08e0d031d7
Krishna Sai <91664773+krishna-github-59@users.noreply.github.com>
2023-10-10 19:19:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -53,7 +53,7 @@ function animatePress(currentColour){
   $("#" + currentColour).addClass("pressed");
   setTimeout(function(){
     $("#" + currentColour).removeClass("pressed");
-  }, 1000); // Adjust the delay duration as needed
+  }, 300); // Adjust the delay duration as needed
 }
 
 
